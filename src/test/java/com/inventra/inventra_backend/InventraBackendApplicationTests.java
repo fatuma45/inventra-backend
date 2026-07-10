632a5327ec63bdbb2058b1da41c2c533feb1f3df
S-mureu <mureushawn67@gmail.com>
2026-07-10 12:32:45 +0300

    Initial Spring Boot project setup

---
+++ src/test/java/com/inventra/inventra_backend/InventraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventra.inventra_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
